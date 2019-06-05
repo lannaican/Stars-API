97cff834dfc6fd9bff68195869434c5b990e106f
Stars <785981017@qq.com>
2019-06-05 14:32:42 +0800

    优化回调线程

--- api/src/main/java/com/star/api/socket/SocketListener.java
+++ api/src/main/java/com/star/api/socket/SocketListener.java
@@ -1,5 +1,9 @@
 package com.star.api.socket;
 
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+
 import com.google.gson.reflect.TypeToken;
 import com.star.annotation.Field;
 import com.star.api.GsonUtil;
@@ -22,21 +26,23 @@ public abstract class SocketListener extends WebSocketListener {
 
     private Object receiver;
     private Type resultType;
+    private Handler handler;
 
     public SocketListener(Object receiver) {
         super();
         this.receiver = receiver;
         this.resultType = new TypeToken<Map<String, Object>>(){}.getType();
+        handler = new Handler(Looper.getMainLooper());
     }
 
     @Override
     public void onMessage(WebSocket webSocket, String text) {
         Map<String, Object> result = GsonUtil.fromJson(text, resultType);
-        for (Method method : receiver.getClass().getMethods()) {
+        for (final Method method : receiver.getClass().getMethods()) {
             SocketReceiver sr = method.getAnnotation(SocketReceiver.class);
             if (sr != null && sr.value().equals(result.get("url"))) {
                 Annotation[][] annotations = method.getParameterAnnotations();
-                Object[] params = new Object[annotations.length];
+                final Object[] params = new Object[annotations.length];
                 for (int i=0; i<annotations.length; i++) {
                     Annotation[] annotation = annotations[i];
                     if (annotation.length > 0 && annotation[0] instanceof Field) {
@@ -50,13 +56,19 @@ public abstract class SocketListener extends WebSocketListener {
                         params[i] = param;
                     }
                 }
-                try {
-                    method.invoke(receiver, params);
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();
-                } catch (InvocationTargetException e) {
-                    e.printStackTrace();
-                }
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            method.invoke(receiver, params);
+                        } catch (IllegalAccessException e) {
+                            e.printStackTrace();
+                        } catch (InvocationTargetException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                return;
             }
         }
     }
