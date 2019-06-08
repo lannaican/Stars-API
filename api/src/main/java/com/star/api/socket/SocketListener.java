1d6886a90977937e079a98aea743cf2993f75bc5
Stars <785981017@qq.com>
2019-06-09 04:55:19 +0800

    优化Socket泛型转换

--- api/src/main/java/com/star/api/socket/SocketListener.java
+++ api/src/main/java/com/star/api/socket/SocketListener.java
@@ -9,9 +9,14 @@ import com.star.annotation.Field;
 import com.star.api.GsonUtil;
 
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Array;
+import java.lang.reflect.GenericArrayType;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.lang.reflect.TypeVariable;
+import java.lang.reflect.WildcardType;
 import java.util.Map;
 
 import okhttp3.WebSocket;
@@ -48,7 +53,8 @@ public abstract class SocketListener extends WebSocketListener {
                     if (annotation.length > 0 && annotation[0] instanceof Field) {
                         String key = ((Field)annotation[0]).value();
                         String param = result.get(key);
-                        Type type = method.getParameterTypes()[i];
+                        Type type = getRawType(method.getParameterTypes()[i]);
+                        type = getParameterUpperBound(0, (ParameterizedType) type);
                         params[i] = GsonUtil.fromJson(param, type);
                     }
                 }
@@ -67,5 +73,48 @@ public abstract class SocketListener extends WebSocketListener {
                 return;
             }
         }
+    }
+
+    private Class<?> getRawType(Type type) {
+        if (type instanceof Class<?>) {
+            // Type is a normal class.
+            return (Class<?>) type;
+        }
+        if (type instanceof ParameterizedType) {
+            ParameterizedType parameterizedType = (ParameterizedType) type;
+
+            // I'm not exactly sure why getRawType() returns Type instead of Class. Neal isn't either but
+            // suspects some pathological case related to nested classes exists.
+            Type rawType = parameterizedType.getRawType();
+            if (!(rawType instanceof Class)) throw new IllegalArgumentException();
+            return (Class<?>) rawType;
+        }
+        if (type instanceof GenericArrayType) {
+            Type componentType = ((GenericArrayType) type).getGenericComponentType();
+            return Array.newInstance(getRawType(componentType), 0).getClass();
+        }
+        if (type instanceof TypeVariable) {
+            // We could use the variable's bounds, but that won't work if there are multiple. Having a raw
+            // type that's more general than necessary is okay.
+            return Object.class;
+        }
+        if (type instanceof WildcardType) {
+            return getRawType(((WildcardType) type).getUpperBounds()[0]);
+        }
+        throw new IllegalArgumentException("Expected a Class, ParameterizedType, or "
+                + "GenericArrayType, but <" + type + "> is of type " + type.getClass().getName());
+    }
+
+    private Type getParameterUpperBound(int index, ParameterizedType type) {
+        Type[] types = type.getActualTypeArguments();
+        if (index < 0 || index >= types.length) {
+            throw new IllegalArgumentException(
+                    "Index " + index + " not in range [0," + types.length + ") for " + type);
+        }
+        Type paramType = types[index];
+        if (paramType instanceof WildcardType) {
+            return ((WildcardType) paramType).getUpperBounds()[0];
+        }
+        return paramType;
     }
 }
