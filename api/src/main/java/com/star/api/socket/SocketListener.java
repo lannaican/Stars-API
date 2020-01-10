00abc6fbc55432345db729e36290029fbb693c03
Stars <785981017@qq.com>
2020-01-10 17:03:16 +0800

    Receiver改为List

--- api/src/main/java/com/star/api/socket/SocketListener.java
+++ api/src/main/java/com/star/api/socket/SocketListener.java
@@ -110,7 +110,6 @@ public abstract class SocketListener extends WebSocketListener {
                             }
                         }
                     });
-                    return;
                 }
             }
         }
