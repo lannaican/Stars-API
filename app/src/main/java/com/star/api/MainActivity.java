327ce9ec206c9abfa89737650754cdc8478c41c6
Stars <785981017@qq.com>
2020-05-11 16:52:29 +0800

    修复注解器不执行问题

--- app/src/main/java/com/star/api/MainActivity.java
+++ app/src/main/java/com/star/api/MainActivity.java
@@ -1,7 +1,8 @@
 package com.star.api;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
